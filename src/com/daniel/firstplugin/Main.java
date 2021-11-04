7310f78a1708f2e164a70a55829978462660c7a5
Daniel <itzdanielmichael@gmail.com>
2021-11-03 22:19:24 -0400

    add command to heal player, add verify handler

--- src/com/daniel/firstplugin/Main.java
+++ src/com/daniel/firstplugin/Main.java
@@ -1,6 +1,10 @@
 package com.daniel.firstplugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.ChatColor;
 
 public class Main extends JavaPlugin {
 
@@ -8,11 +12,44 @@ public class Main extends JavaPlugin {
 	public void onEnable() {
 	// when the plugin is ran for first time it will run this block of code.
 		
-		System.out.println("First plugin has worked!");
+	  System.out.println("PLUGIN ENABLED!");
 	}
 
 	@Override
 	public void onDisable() {
+	  System.out.println("PLUGIN DISABLED!");
+	}
+	
+	//	onCommand is a method that runs when any player runs ANY command (even if it doesn't exist.) EX: /hello
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+		if (cmd.getName().equals("recover")) { // <-- a "/" isn't needed
+			Player player = verifyPlayer(sender);
+			if (player != null) {
+				healPlayer(player);
+			}
+		}
+
+		return false;
+	}
+	
+
+	public Player verifyPlayer(CommandSender sender) {
+		if (sender instanceof Player) {
+		  Player player = (Player) sender;
+		  return player;
+		}
 		
+		System.out.println("You cannot use this console through console");
+		return null;
+	}
+	
+	public void healPlayer(Player playerToBeHealed) {
+		playerToBeHealed.sendMessage(
+		  ChatColor.GRAY + "Hello, " + ChatColor.GREEN + playerToBeHealed.getName() + 
+		  ChatColor.GRAY + ". Your health has been restored"
+		);
+	
+		playerToBeHealed.setHealth(20.0);
 	}
 }
