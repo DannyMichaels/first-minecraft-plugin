638f5f3f9e69a2fed767d0f8573572357f21551a
Daniel <itzdanielmichael@gmail.com>
2021-11-03 22:37:06 -0400

    format code with eclipse

--- src/com/daniel/firstplugin/Main.java
+++ src/com/daniel/firstplugin/Main.java
@@ -10,59 +10,58 @@ public class Main extends JavaPlugin {
 
 	@Override
 	public void onEnable() {
-	// when the plugin is ran for first time it will run this block of code.
-		
-	  System.out.println("PLUGIN ENABLED!");
+		// when the plugin is ran for first time it will run this block of code.
+
+		System.out.println("PLUGIN ENABLED!");
 	}
 
 	@Override
 	public void onDisable() {
-	  System.out.println("PLUGIN DISABLED!");
+		System.out.println("PLUGIN DISABLED!");
 	}
-	
-	//  onCommand is a method that runs when any player runs ANY command (even if it doesn't exist.) EX: /hello
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-	  String commandName = cmd.getName();
-	  Player player = verifyPlayer(sender);
 
-	  if (player == null) return false;
-			  
-	  switch(commandName) {
-	    case "recover": 
-		  healPlayer(player);
-			
-	    case "reverse_name":
-		    printPlayerNameReversed(player);
-		  
-		 default: 
+	// onCommand is a method that runs when any player runs ANY command (even if it
+	// doesn't exist.) EX: /hello
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		String commandName = cmd.getName();
+		Player player = verifyPlayer(sender);
+
+		if (player == null)
+			return false;
+
+		switch (commandName) {
+		case "recover":
+			healPlayer(player);
+
+		case "reverse_name":
+			printPlayerNameReversed(player);
+
+		default:
 			break;
-	  }
-	  
-	  return false;
+		}
+
+		return false;
 	}
-	
 
 	public Player verifyPlayer(CommandSender sender) {
 		if (sender instanceof Player) {
-		  Player player = (Player) sender;
-		  return player;
+			Player player = (Player) sender;
+			return player;
 		}
-		
+
 		System.out.println("You cannot use this console through console");
 		return null;
 	}
-	
+
 	public void healPlayer(Player playerToBeHealed) {
-	  playerToBeHealed.sendMessage(
-		ChatColor.GRAY + "Hello, " + ChatColor.GREEN + playerToBeHealed.getName() + 
-		ChatColor.GRAY + ". Your health has been restored"
-	   );
-	
-	  playerToBeHealed.setHealth(20.0);
+		playerToBeHealed.sendMessage(ChatColor.GRAY + "Hello, " + ChatColor.GREEN + playerToBeHealed.getName()
+				+ ChatColor.GRAY + ". Your health has been restored");
+
+		playerToBeHealed.setHealth(20.0);
 	}
-	
+
 	public void printPlayerNameReversed(Player player) {
-	  String reversedPlayerName = new StringBuilder(player.getName()).reverse().toString();
-	  player.sendMessage(reversedPlayerName);
+		String reversedPlayerName = new StringBuilder(player.getName()).reverse().toString();
+		player.sendMessage(reversedPlayerName);
 	}
 }
